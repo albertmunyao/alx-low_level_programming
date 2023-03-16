18899e6269e8fe3c22345cc3855f85adf1f6e7ec
albertmunyao <albertmunyao22@gmail.com>
2023-03-16 05:22:12 -0700

    4-puts.c

--- 0x00-hello_world/4-puts.c
+++ 0x00-hello_world/4-puts.c
@@ -1,13 +1,10 @@
 #include <stdio.h>
-
 /**
-*main - this is the main function
-*
-*Return - always 0
-*/
-
+  * main - Outputs a string
+  * Return: 0 returned to exit the program
+  */
 int main(void)
-{    
-	puts("\"Programming is like building a multilingual puzzle);
-	return(0);
+{
+	puts("\"Programming is like building a multilingual puzzle");
+	return (0);
 }
