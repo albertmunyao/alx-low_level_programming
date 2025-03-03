e444bded92e6f975980812b5c8d4b9fc7aa83fd5
albert <albertmunyao222gmail.com>
2025-03-03 05:38:40 -0800

    2-add_dnodeint.c

--- 0x17-doubly_linked_lists/2-add_dnodeint.c
+++ 0x17-doubly_linked_lists/2-add_dnodeint.c
@@ -2,7 +2,7 @@
 #include"lists.h"
 
 /**
- * *add _dnoident - adds a new node at the beginning 
+ * add _dnoident - adds a new node at the beginning 
  * @dlistint_t: location of the  header list
  * @n: The  data to store in the new node
  *
@@ -15,7 +15,7 @@ dlistint_t *add_dnodeint(dlistint_t **head, const int n)
 	dlistint_t *node;
 
 /*Alocate memory for the node*/
-node (dlistint_t) = malloc(sizeof(dlistint_t));
+node = malloc(sizeof(dlistint_t));
 
 if(node == NULL)
 	return (NULL);
